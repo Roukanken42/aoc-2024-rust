dc005e72abd96fc8b846b9c2211388fa5324832a
Roukanken <kuko0411@gmail.com>
2024-12-01 11:59:41 +0100

    Day 01: part 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,20 +1,31 @@
 use advent_of_code::utils::{parse_input_by_lines, Parsable};
+use itertools::Itertools;
 use nom::character::complete::space1;
 use nom::sequence::separated_pair;
 use nom::IResult;
 
 advent_of_code::solution!(1);
 
-pub fn parse(input: &str) -> IResult<&str, Vec<(u64, u64)>> {
-    let parse_line = separated_pair(u64::parse, space1, u64::parse);
+pub fn parse(input: &str) -> IResult<&str, Vec<(i64, i64)>> {
+    let parse_line = separated_pair(i64::parse, space1, i64::parse);
     parse_input_by_lines(parse_line)(input)
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
-    None
+pub fn part_one(input: &str) -> Option<i64> {
+    let (_, input) = parse(input).ok()?;
+
+    let left: Vec<_> = input.iter().map(|(l, _)| l).sorted().collect();
+    let right: Vec<_> = input.iter().map(|(_, r)| r).sorted().collect();
+
+    Some(
+        left.into_iter()
+            .zip(right.into_iter())
+            .map(|(&l, &r)| (l - r).abs())
+            .sum::<i64>(),
+    )
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+pub fn part_two(input: &str) -> Option<i64> {
     None
 }
 
@@ -26,13 +37,16 @@ mod tests {
     fn test_parse() {
         let input = advent_of_code::template::read_file("examples", DAY);
         let result = parse(&input);
-        assert_eq!(result, Ok(("", vec![(3, 4), (4, 3), (2, 5), (1, 3), (3, 9), (3, 3)])));
+        assert_eq!(
+            result,
+            Ok(("", vec![(3, 4), (4, 3), (2, 5), (1, 3), (3, 9), (3, 3)]))
+        );
     }
 
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(11));
     }
 
     #[test]
