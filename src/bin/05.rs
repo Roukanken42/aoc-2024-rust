4b629869098c23e0142b28e49ab0966f57e17976
Roukanken <kuko0411@gmail.com>
2024-12-05 17:35:57 +0100

    Day 05: parse

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,4 +1,44 @@
+use advent_of_code::utils::{end_of_file, Parsable};
+use nom::bytes::complete::tag;
+use nom::character::complete::line_ending;
+use nom::combinator::map;
+use nom::multi::{many1, separated_list1};
+use nom::sequence::separated_pair;
+use nom::IResult;
+
 advent_of_code::solution!(5);
+
+#[derive(Debug, PartialEq)]
+struct Rule {
+    lesser: u32,
+    greater: u32,
+}
+
+impl Parsable<'_> for Rule {
+    fn parse(input: &str) -> IResult<&str, Self> {
+        let pair = separated_pair(u32::parse, tag("|"), u32::parse);
+        map(pair, |(lesser, greater)| Rule { lesser, greater })(input)
+    }
+}
+
+struct Input {
+    rules: Vec<Rule>,
+    updates: Vec<Vec<u32>>,
+}
+
+impl Parsable<'_> for Input {
+    fn parse(input: &'_ str) -> IResult<&'_ str, Self> {
+        let (input, rules) = separated_list1(line_ending, Rule::parse)(input)?;
+        let (input, _) = many1(line_ending)(input)?;
+
+        let parse_update = separated_list1(tag(","), u32::parse);
+        let (input, updates) = separated_list1(line_ending, parse_update)(input)?;
+
+        let (input, _) = end_of_file(input)?;
+
+        Ok((input, Input { rules, updates }))
+    }
+}
 
 pub fn part_one(input: &str) -> Option<u32> {
     None
@@ -11,6 +51,32 @@ pub fn part_two(input: &str) -> Option<u32> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_parse() {
+        let input = advent_of_code::template::read_file("examples", DAY);
+        let result = Input::parse(&input);
+
+        let (str, input) = result.unwrap();
+
+        assert!(str.is_empty());
+        assert_eq!(
+            input.rules[0],
+            Rule {
+                lesser: 47,
+                greater: 53
+            }
+        );
+        assert_eq!(
+            input.rules.last(),
+            Some(&Rule {
+                lesser: 53,
+                greater: 13
+            })
+        );
+
+        assert_eq!(input.updates[0], vec![75, 47, 61, 53, 29]);
+    }
 
     #[test]
     fn test_part_one() {
