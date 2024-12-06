b40e92dbc58a4a9aae6ab4222c125370508fe502
Roukanken <kuko0411@gmail.com>
2024-12-06 11:04:51 +0100

    Day 06: parse

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,4 +1,31 @@
+use advent_of_code::utils::{parse_input_by_lines, Parsable};
+use nom::branch::alt;
+use nom::bytes::complete::tag;
+use nom::combinator::value;
+use nom::multi::many1;
+
 advent_of_code::solution!(6);
+
+#[derive(Debug, PartialEq, Clone)]
+enum Tile {
+    Empty,
+    Obstacle,
+    Guard,
+}
+
+impl Parsable<'_> for Tile {
+    fn parse(input: &str) -> nom::IResult<&str, Self> {
+        alt((
+            value(Tile::Empty, tag(".")),
+            value(Tile::Obstacle, tag("#")),
+            value(Tile::Guard, tag("^")),
+        ))(input)
+    }
+}
+
+fn parse(input: &str) -> nom::IResult<&str, Vec<Vec<Tile>>> {
+    parse_input_by_lines(many1(Tile::parse))(input)
+}
 
 pub fn part_one(_input: &str) -> Option<u32> {
     None
@@ -11,6 +38,20 @@ pub fn part_two(_input: &str) -> Option<u32> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_parse() {
+        let input = advent_of_code::template::read_file("examples", DAY);
+        let result = parse(&input);
+
+        let (str, input) = result.unwrap();
+
+        assert!(str.is_empty());
+        assert_eq!(
+            input[0][4],
+            Tile::Obstacle,
+        );
+    }
 
     #[test]
     fn test_part_one() {
