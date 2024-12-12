3d8d591a9f99a077e2448c11c7b4c8a7244c4e51
Roukanken <kuko0411@gmail.com>
2024-12-12 09:34:23 +0100

    Day 12: parse

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -1,4 +1,14 @@
+use advent_of_code::utils::parse_input_by_lines;
+use nom::bytes::complete::take_while;
+use nom::combinator::map;
+use nom::IResult;
+
 advent_of_code::solution!(12);
+
+fn parse(input: &str) -> IResult<&str, Vec<Vec<char>>> {
+    let line = map(take_while(|c| ('A'..='Z').contains(&c)), |line: &str| line.chars().collect());
+    parse_input_by_lines(line)(input)
+}
 
 pub fn part_one(input: &str) -> Option<u32> {
     None
@@ -11,6 +21,16 @@ pub fn part_two(input: &str) -> Option<u32> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_parse() {
+        let input = advent_of_code::template::read_file("examples", DAY);
+        let (rest, result) = parse(&input).unwrap();
+
+        assert!(rest.is_empty());
+        assert_eq!(result[0], vec!['R', 'R', 'R', 'R', 'I', 'I', 'C', 'C', 'F', 'F']);
+        assert_eq!(result[2], vec!['V', 'V', 'R', 'R', 'R', 'C', 'C', 'F', 'F', 'F']);
+    }
 
     #[test]
     fn test_part_one() {
