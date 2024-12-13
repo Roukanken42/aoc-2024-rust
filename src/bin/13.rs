b8cd72e05ad0600f1e0af12f7aeeff7fde036733
Roukanken <kuko0411@gmail.com>
2024-12-13 10:17:29 +0100

    Day 13: cleanup

--- src/bin/13.rs
+++ src/bin/13.rs
@@ -2,7 +2,8 @@ use advent_of_code::utils::location::Location;
 use advent_of_code::utils::{parse_input_by_lines, Parsable};
 use nom::bytes::complete::tag;
 use nom::character::complete::{char, line_ending};
-use nom::sequence::delimited;
+use nom::combinator::map;
+use nom::sequence::{delimited, pair, preceded, separated_pair};
 use nom::IResult;
 
 advent_of_code::solution!(13);
@@ -24,19 +25,12 @@ impl Parsable<'_> for Machine {
     }
 }
 
-fn parse_location<'a>(sep: char) -> impl FnMut(&'a str) -> IResult<&'a str, Location<i64>> {
+fn parse_location(sep: char) -> impl FnMut(&str) -> IResult<&str, Location<i64>> {
     move |input: &str| {
-        let (input, _) = tag("X")(input)?;
-        let (input, _) = char(sep)(input)?;
-        let (input, x) = i64::parse(input)?;
+        let parse_x = preceded(pair(tag("X"), char(sep)), i64::parse);
+        let parse_y = preceded(pair(tag("Y"), char(sep)), i64::parse);
 
-        let (input, _) = tag(", ")(input)?;
-
-        let (input, _) = tag("Y")(input)?;
-        let (input, _) = char(sep)(input)?;
-        let (input, y) = i64::parse(input)?;
-
-        Ok((input, Location::new(x, y)))
+        map(separated_pair(parse_x, tag(", "), parse_y), |(x, y)| Location::new(x, y))(input)
     }
 }
 
@@ -48,10 +42,10 @@ fn solve(a: Location<i64>, b: Location<i64>, p: Location<i64>) -> Option<i64> {
     let b_count = (a.y * p.x - a.x * p.y) / (b.x * a.y - b.y * a.x);
     let a_count = (p.x - b.x * b_count) / a.x;
 
-    if (a * a_count + b * b_count) != p {
-        None
-    } else {
+    if (a * a_count + b * b_count) == p {
         Some(a_count * 3 + b_count)
+    } else {
+        None
     }
 }
 
@@ -68,17 +62,12 @@ pub fn part_one(input: &str) -> Option<i64> {
 
 pub fn part_two(input: &str) -> Option<i64> {
     let (_, machines) = parse(input).unwrap();
+    let raise = Location::new(10000000000000, 10000000000000);
 
     Some(
         machines
             .iter()
-            .flat_map(|machine| {
-                solve(
-                    machine.button_a,
-                    machine.button_b,
-                    machine.prize + Location::new(10000000000000, 10000000000000),
-                )
-            })
+            .flat_map(|machine| solve(machine.button_a, machine.button_b, machine.prize + raise))
             .sum(),
     )
 }
