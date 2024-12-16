d171860a32b6f06db91654bb9f29f00dd7179fcc
Roukanken <kuko0411@gmail.com>
2024-12-16 08:02:32 +0100

    Day 16: scaffold

---
+++ src/bin/16.rs
@@ -0,0 +1,32 @@
+advent_of_code::solution!(16);
+
+pub fn part_one(input: &str) -> Option<u32> {
+    None
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one_small() {
+        let result = part_one(&advent_of_code::template::read_file_part("examples", DAY, 1));
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_part_one_large() {
+        let result = part_one(&advent_of_code::template::read_file_part("examples", DAY, 2));
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file_part("examples", DAY, 2));
+        assert_eq!(result, None);
+    }
+}
