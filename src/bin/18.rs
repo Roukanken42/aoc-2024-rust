9f08c9eee68d3f71a2a874fe023260ba843b56c6
Roukanken <kuko0411@gmail.com>
2024-12-18 09:25:42 +0100

    Day 18: part 1

--- src/bin/18.rs
+++ src/bin/18.rs
@@ -1,9 +1,13 @@
 use advent_of_code::utils::location::Location;
 use advent_of_code::utils::{parse_input_by_lines, Parsable};
+use hashbrown::HashSet;
 use nom::character::complete::char;
 use nom::combinator::map;
 use nom::sequence::separated_pair;
 use nom::IResult;
+use num::Zero;
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
 
 advent_of_code::solution!(18);
 
@@ -12,11 +16,39 @@ fn parse(input: &str) -> IResult<&str, Vec<Location<i32>>> {
     parse_input_by_lines(parse_location)(input)
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
+pub fn part_one(input: &str) -> Option<i32> {
     part_one_inner(input, Location::new(70, 70), 1024)
 }
 
-fn part_one_inner(input: &str, size: Location<i32>, simulate: usize) -> Option<u32> {
+fn part_one_inner(input: &str, size: Location<i32>, simulate: usize) -> Option<i32> {
+    let (_, bytes) = parse(input).unwrap();
+    let map = bytes.iter().take(simulate).cloned().collect::<HashSet<_>>();
+    let range = Location::zero().square_range(size + Location::new(1, 1));
+
+    let mut queue = BinaryHeap::new();
+    let mut visited = HashSet::new();
+
+    queue.push(Reverse((size.manhattan_distance(Location::zero()), 0, Location::new(0, 0))));
+
+    while let Some(Reverse((_, steps, loc))) = queue.pop() {
+        if visited.contains(&loc) {
+            continue;
+        }
+        visited.insert(loc);
+
+        if loc == size {
+            return Some(steps);
+        }
+
+        for neighbour_loc in loc.iter_adjacent() {
+            if map.contains(&neighbour_loc) || !range.contains(&neighbour_loc) {
+                continue;
+            }
+
+            queue.push(Reverse((size.manhattan_distance(neighbour_loc) + steps + 1, steps + 1, neighbour_loc)));
+        }
+    }
+
     None
 }
 
@@ -41,8 +73,8 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let result = part_one_inner(&advent_of_code::template::read_file("examples", DAY), Location::new(7, 7), 12);
-        assert_eq!(result, None);
+        let result = part_one_inner(&advent_of_code::template::read_file("examples", DAY), Location::new(6, 6), 12);
+        assert_eq!(result, Some(22));
     }
 
     #[test]
