1f4c6e68fdf8dbec896318ff41a47bc0ec315420
Roukanken <kuko0411@gmail.com>
2024-12-20 10:44:50 +0100

    Day 20: parse

--- src/bin/20.rs
+++ src/bin/20.rs
@@ -1,4 +1,34 @@
+use advent_of_code::utils::{parse_input_by_lines, Parsable};
+use nom::branch::alt;
+use nom::character::complete::char;
+use nom::combinator::value;
+use nom::multi::many1;
+use nom::IResult;
+
 advent_of_code::solution!(20);
+
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum Tile {
+    Wall,
+    Empty,
+    Start,
+    End,
+}
+
+impl Parsable<'_> for Tile {
+    fn parse(input: &str) -> IResult<&str, Self> {
+        alt((
+            value(Tile::Wall, char('#')),
+            value(Tile::Empty, char('.')),
+            value(Tile::Start, char('S')),
+            value(Tile::End, char('E')),
+        ))(input)
+    }
+}
+
+fn parse(input: &str) -> IResult<&str, Vec<Vec<Tile>>> {
+    parse_input_by_lines(many1(Tile::parse))(input)
+}
 
 pub fn part_one(input: &str) -> Option<u32> {
     None
@@ -11,6 +41,21 @@ pub fn part_two(input: &str) -> Option<u32> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_parse() {
+        let input = "S.#\n\
+                     #.E";
+        let result = parse(input);
+
+        assert_eq!(
+            result,
+            Ok((
+                "",
+                vec![vec![Tile::Start, Tile::Empty, Tile::Wall], vec![Tile::Wall, Tile::Empty, Tile::End],]
+            ))
+        );
+    }
 
     #[test]
     fn test_part_one() {
